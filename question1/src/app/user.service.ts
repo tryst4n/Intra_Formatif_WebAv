a0e744b3a2f9ae0625a7f78c8f92cb8e358f37dd
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2025-03-10 13:49:17 -0400

    redirect au login après déconnexion

--- question1/src/app/user.service.ts
+++ question1/src/app/user.service.ts
@@ -2,32 +2,30 @@ import { Injectable } from '@angular/core';
 import { User } from './user';
 import { Router } from '@angular/router';
 
-const USER_KEY = "user"
+const USER_KEY = 'user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   currentUser?: User;
 
   constructor(public route: Router) {
     let userString = localStorage.getItem(USER_KEY);
-    if(userString != null)
-      this.currentUser = JSON.parse(userString);
+    if (userString != null) this.currentUser = JSON.parse(userString);
   }
 
-  connect(user:User){
+  connect(user: User) {
     this.currentUser = user;
     localStorage.setItem(USER_KEY, JSON.stringify(user));
 
-    this.route.navigate(['/home'])
+    this.route.navigate(['/home']);
   }
 
-  disconnect(){
+  disconnect() {
     this.currentUser = undefined;
     localStorage.removeItem(USER_KEY);
 
-    this.route.navigate(['/home'])
+    this.route.navigate(['/login']);
   }
 }
